37063777f3e30f3332d3a0d7e71347d2fef7dc0d
Andrey1303110 <andrey1303110@gmail.com>
2020-11-12 16:50:47 +0200

    add js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*console.log("Hello World!");
+
+function fn() {
+    console.log("Pressed");
+}
+
+var a = prompt("A = ", 42);
+alert(`A = ${a}`);
+*/
+
+var n = 10;
+var f = 'test'
+
+console.log(typeof (n));
+console.log(typeof (f));
