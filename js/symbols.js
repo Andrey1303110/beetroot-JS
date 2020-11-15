c5bf37595e28695389274c17a64dddc52988aefc
Andrey1303110 <andrey1303110@gmail.com>
2020-11-15 21:56:50 +0200

    add js symbols

---
+++ js/symbols.js
@@ -0,0 +1,30 @@
+var sym = prompt("Введите любую цифру от 1 до 9 пжлст = ", '');
+if (sym == 1) {
+    alert('!');
+}
+if (sym == 2) {
+    alert('@');
+}
+if (sym == 3) {
+    alert('#');
+}
+if (sym == 4) {
+    alert('$');
+}
+if (sym == 5) {
+    alert('%');
+}
+if (sym == 6) {
+    alert('^');
+}
+if (sym == 7) {
+    alert('&');
+}
+if (sym == 8) {
+    alert('*');
+}
+if (sym == 9) {
+    alert('(');
+}
+
+console.log(sym);
