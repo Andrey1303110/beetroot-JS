e429ee8734ce8751368098ca6b5e94f9044cd7fb
Andrey1303110 <andrey1303110@gmail.com>
2020-11-15 20:00:19 +0200

    add var age

--- js/main.js
+++ js/main.js
@@ -1,15 +1,24 @@
-/*console.log("Hello World!");
-
-function fn() {
-    console.log("Pressed");
+var age = prompt("Введите ваш возраст пжлст = ", '');
+if (age >= Infinity) {
+    alert('Сам то понял что хотел?');
+}
+if (age >= 120 && age < Infinity) {
+    alert('Фигню якусь написав');
+}
+if (age >= 60 && age < 120) {
+    alert('Ты таки старый');
+}
+if (age >= 18 && age < 60) {
+    alert('Ты взрослый');
+}
+if (age >= 12 && age < 18) {
+    alert('Ты подросток');
+}
+if (age > 2 && age < 12) {
+    alert('Ты почти подросток');
+}
+if (age <= 2) {
+    alert('Ты дитё ещё');
 }
 
-var a = prompt("A = ", 42);
-alert(`A = ${a}`);
-*/
-
-var n = 10;
-var f = 'test'
-
-console.log(typeof (n));
-console.log(typeof (f));
+console.log(age);
