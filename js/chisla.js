22d4778906bbf2874c730385c939db10f4c5adc2
Andrey1303110 <andrey1303110@gmail.com>
2020-11-16 20:43:11 +0200

    commit

---
+++ js/chisla.js
@@ -0,0 +1,82 @@
+var a, b, c;
+a = prompt("Enter first number");
+b = prompt("Enter second number");
+c = prompt("Enter third number");
+if (a == b && b == c) {
+    if (a == b) {
+        document.write(a);
+        document.write(" = ");
+        document.write(b);
+    }
+    if (b == c) {
+        document.write(" = ");
+        document.write(c);
+    }
+}
+if (a == b && b > c) {
+    if (a == b) {
+        document.write(a);
+        document.write(" = ");
+        document.write(b);
+    }
+    if (b > c) {
+        document.write(" > ");
+        document.write(c);
+    }
+}
+if (a > b && b == c) {
+    if (a > b) {
+        document.write(a);
+        document.write(" > ");
+        document.write(b);
+    }
+    if (b == c) {
+        document.write(" = ");
+        document.write(c);
+    }
+}
+if (a < b && b < c) {
+    if (a < b) {
+        document.write(a);
+        document.write(" < ");
+        document.write(b);
+    }
+    if (b < c) {
+        document.write(" < ");
+        document.write(c);
+    }
+}
+if (a > b && b < c) {
+    if (a > b) {
+        document.write(a);
+        document.write(" > ");
+        document.write(b);
+    }
+    if (b > c) {
+        document.write(" < ");
+        document.write(c);
+    }
+}
+if (a < b && b == c) {
+    if (a > b) {
+        document.write(a);
+        document.write(" < ");
+        document.write(b);
+    }
+    if (b == c) {
+        document.write(" = ");
+        document.write(c);
+    }
+}
+if (a < b && b > c) {
+    if (a < b) {
+        document.write(a);
+        document.write(" < ");
+        document.write(b);
+    }
+    if (b > c) {
+        document.write(" > ");
+        document.write(c);
+    }
+}
+
--- js/symbols.js
+++ js/symbols.js
@@ -1,30 +1,28 @@
 var sym = prompt("Введите любую цифру от 1 до 9 пжлст = ", '');
-if (sym == 1) {
-    alert('!');
+if (sym >= 10) {
+    alert('Введите чило до 10')
+    sym = prompt("Введите любую цифру от 1 до 9 пжлст = ", '');
 }
-if (sym == 2) {
-    alert('@');
-}
-if (sym == 3) {
-    alert('#');
-}
-if (sym == 4) {
-    alert('$');
-}
-if (sym == 5) {
-    alert('%');
-}
-if (sym == 6) {
-    alert('^');
-}
-if (sym == 7) {
-    alert('&');
-}
-if (sym == 8) {
-    alert('*');
-}
-if (sym == 9) {
-    alert('(');
+if (sym < 10) {
+    if (sym == 1)
+        alert('!');
+    if (sym == 2)
+        alert('@');
+    if (sym == 3)
+        alert('#');
+    if (sym == 4)
+        alert('$');
+    if (sym == 5)
+        alert('%');
+    if (sym == 6)
+        alert('^');
+    if (sym == 7)
+        alert('&');
+    if (sym == 8)
+        alert('*');
+    if (sym == 9)
+        alert('(');
+    alert('Это был Ваш символ');
 }
 
 console.log(sym);
