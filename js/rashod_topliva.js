43496ebbd65bd45a8865c997c7ef9da210e01904
Andrey1303110 <andrey1303110@gmail.com>
2020-11-29 20:22:57 +0200

    commit

--- js/rashod_topliva.js
+++ js/rashod_topliva.js
@@ -22,6 +22,8 @@ function rashodTopliva(rashod) {
     priceFor1Km = (litr * gasprice) / km;
     alert("И так " + Name + ", расход топлива твоего " + avto + " составляет " + (rashod.toFixed(1)) + " литров на 100км");
     alert("Соответсвенно " + Name + ", на 1 км пройденного пути на " + avto + " ты потратил(а) " + (priceFor1Km.toFixed(2)) + " гривен");
+    document.write("И так " + Name + ", расход топлива твоего " + avto + " составляет " + (rashod.toFixed(1)) + " литров на 100км<br />");
+    document.write("Соответсвенно " + Name + ", на 1 км пройденного пути на " + avto + " ты потратил(а) " + (priceFor1Km.toFixed(2)) + " гривен");
 }
 
 rashodTopliva(rashod);
